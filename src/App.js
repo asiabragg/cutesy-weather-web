6647f3d46e60426bb2324c1cb88226668f178cf4
Asia Bragg <asiambragg987@gmail.com>
2020-10-17 01:44:24 -0700

    Added Additional HTML & CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 // API Declaration Here
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+    "Friday", "Saturday"]
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${month}, ${date}, ${year} `
+
+  }
+
   return (
     <div className="app">
       <main>
@@ -11,6 +27,16 @@ function App() {
             className="search-bar"
             placeholder="Search here..."
           />
+        </div>
+
+        <div className="location-box">
+          <div className="location">Garden Grove, CA</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">70°F</div>
+          <div className="weather">Cloudy</div>
         </div>
       </main>
     </div>
